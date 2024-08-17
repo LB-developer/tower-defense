9bb2fdf54d46bb58d919bcd3ebd9ac2edf4eddcf
POR7ALa <thatsair@gmail.com>
2024-08-18 09:25:17 +1200

    refactor checkTowerSelectorBoundary > calcPosition

--- client/components/Towers/TowerSelector.tsx
+++ client/components/Towers/TowerSelector.tsx
@@ -10,18 +10,14 @@ function TowerSelector({ wallSelected, mouseXY }: TowerSelectorProps) {
   const windowWidth: number = window.innerWidth
   const towerSelectorWidth: number = windowWidth / 4
   const towerSelectorHeight: number = windowHeight / 3
-  checkTowerSelectorBoundary()
 
-  const towerSelectorStyle: React.CSSProperties = {
-    position: "fixed",
-    top: `${y}px`,
-    left: `${x}px`,
-    minWidth: `${towerSelectorWidth}px`,
-    height: `${towerSelectorHeight}px`,
-    backgroundColor: "red",
-  }
+  function calculatePosition(
+    x: number,
+    y: number
+  ): { top: number; left: number } {
+    let adjustedX = x
+    let adjustedY = x
 
-  function checkTowerSelectorBoundary(): void {
     if (x + towerSelectorWidth > windowWidth) {
       towerSelectorStyle.left = windowWidth - towerSelectorWidth
     }
@@ -29,6 +25,18 @@ function TowerSelector({ wallSelected, mouseXY }: TowerSelectorProps) {
     if (y + towerSelectorHeight > windowHeight) {
       towerSelectorStyle.top = windowHeight - towerSelectorHeight
     }
+
+    return { top: adjustedY, left: adjustedX }
+  }
+
+  const { top, left } = calculatePosition(x, y)
+  const towerSelectorStyle: React.CSSProperties = {
+    position: "fixed",
+    top: `${top}px`,
+    left: `${left}px`,
+    minWidth: `${towerSelectorWidth}px`,
+    height: `${towerSelectorHeight}px`,
+    backgroundColor: "red",
   }
 
   return <>{wallSelected && <div style={towerSelectorStyle}></div>}</>
